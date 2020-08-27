664d5bcfe4d24b6f84b3403282c664937e1ceec5
Richard Hansen <rhansen@rhansen.org>
2020-08-27 17:38:48 -0400

    Use camel case for variable names

--- index.js
+++ index.js
@@ -15,16 +15,16 @@
 const settings = require('ep_etherpad-lite/node/utils/Settings');
 const authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
 
-exports.authenticate = (hook_name, {req}, cb) => {
+exports.authenticate = (hookName, {req}, cb) => {
   console.debug('ep_headerauth.authenticate');
   if (!settings.trustProxy) {
     console.warn(`ep_headerauth.authenticate: Failed authentication from IP ${req.ip} - trustProxy is not enabled`);
     return cb([false]);
   }
-  const {username_header} = settings.headerauth;
-  const username = req.headers[username_header];
+  const hs = settings.headerauth;
+  const username = req.headers[hs.username_header];
   if (!username) {
-    console.warn(`ep_headerauth.authenticate: Failed authentication from IP ${req.ip} - missing ${username_header} header`);
+    console.warn(`ep_headerauth.authenticate: Failed authentication from IP ${req.ip} - missing ${hs.username_header} header`);
     for (const hdr in req.headers) {
       console.debug(`ep_headerauth.authenticate: Header: ${hdr}: ${req.headers[hdr]}`);
     }
@@ -42,7 +42,7 @@ exports.authenticate = (hook_name, {req}, cb) => {
   return cb([true]);
 };
 
-exports.expressConfigure = (hook_name, ctx, cb) => {
+exports.expressConfigure = (hookName, ctx, cb) => {
   if (!('headerauth' in settings)) settings.headerauth = {};
   if (!('username_header' in settings.headerauth))
     settings.headerauth.username_header = 'x-authenticated-user';
@@ -51,7 +51,7 @@ exports.expressConfigure = (hook_name, ctx, cb) => {
   return cb();
 };
 
-exports.handleMessage = async (hook_name, {client: {client: {request: {session}}}, message}) => {
+exports.handleMessage = async (hookName, {client: {client: {request: {session}}}, message}) => {
   console.debug('ep_headerauth.handleMessage');
   if (!('user' in session)) {
     console.debug('ep_headerauth.handleMessage: user info missing from session');
@@ -78,7 +78,7 @@ exports.handleMessage = async (hook_name, {client: {client: {request: {session}}
     return;
   }
   console.debug(`ep_headerauth.handleMessage: getting author ID for token ${token}`);
-  const author_id = await authorManager.getAuthor4Token(token);
-  console.debug(`ep_headerauth.handleMessage: Setting name for ${author_id} to ${displayname}`);
-  authorManager.setAuthorName(author_id, displayname);
+  const authorId = await authorManager.getAuthor4Token(token);
+  console.debug(`ep_headerauth.handleMessage: Setting name for ${authorId} to ${displayname}`);
+  authorManager.setAuthorName(authorId, displayname);
 };
