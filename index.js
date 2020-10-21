efb47cc225d9d726704e73b295518ad77287ebef
Richard Hansen <rhansen@rhansen.org>
2020-10-20 22:14:06 -0400

    Add ESLint global comment

--- index.js
+++ index.js
@@ -12,6 +12,8 @@
 // implied. See the License for the specific language governing
 // permissions and limitations under the License.
 
+/* global exports, require */
+
 const authorManager = require('ep_etherpad-lite/node/db/AuthorManager');
 
 let settings;
