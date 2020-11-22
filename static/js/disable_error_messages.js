65cd6740eb712cbd4ad9ed9ccb90e51aa70fd5a6
John McLear <john@mclear.co.uk>
2020-11-22 16:20:10 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/disable_error_messages.js
+++ static/js/disable_error_messages.js
@@ -1,11 +1,11 @@
 var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
 
 exports.suppress = function (hook_name, args, cb) {
-  setTimeout(function(){
-    $("#editorloadingbox").hide("Error messages suppressed by your system administrator");
-    $("#editorcontainerbox").append("<div id='suppresserror' style='text-align:center;margin-top:50px;'><h1>Error messages suppressed by your system administrator</h1></div>");
+  setTimeout(() => {
+    $('#editorloadingbox').hide('Error messages suppressed by your system administrator');
+    $('#editorcontainerbox').append("<div id='suppresserror' style='text-align:center;margin-top:50px;'><h1>Error messages suppressed by your system administrator</h1></div>");
   }, 1000);
-}
+};
 exports.postAceInit = function (hook_name, args, cb) {
-  $("#suppresserror").hide();
-}
+  $('#suppresserror').hide();
+};
