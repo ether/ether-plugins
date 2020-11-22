d1679bccaee3c8b523415ec6d9af24b34dbe5d79
John McLear <john@mclear.co.uk>
2020-11-22 16:20:41 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/disable_reset_authorship_colours.js
+++ static/js/disable_reset_authorship_colours.js
@@ -1,13 +1,13 @@
 var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
 
 exports.postAceInit = function (hook_name, args, cb) {
-    // get clearauthorship item
-	var item = $('li[data-key="clearauthorship"]');
-	// check if the item is sorrounded by separators, then hide the previous one
-	if (item.prev('.separator').length == 1 &&
+  // get clearauthorship item
+  const item = $('li[data-key="clearauthorship"]');
+  // check if the item is sorrounded by separators, then hide the previous one
+  if (item.prev('.separator').length == 1 &&
 		item.next('.separator').length == 1) {
 	  item.prev('.separator').remove();
-	}	
-	// hide the item
-	item.remove();
-}
+  }
+  // hide the item
+  item.remove();
+};
