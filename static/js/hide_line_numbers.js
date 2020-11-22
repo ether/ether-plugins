3a84358e69114e51366fc64f569cea27c0dc3af9
John McLear <john@mclear.co.uk>
2020-11-22 16:36:12 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/hide_line_numbers.js
+++ static/js/hide_line_numbers.js
@@ -1,8 +1,8 @@
-exports.postAceInit = function(){
+exports.postAceInit = function () {
 //  if($('#options-linenoscheck').is(':checked') || $('#sidediv').is(":visible")) {
 //  console.log("Foo");
 //    $('#options-linenoscheck').click();
 //    $('#options-linenoscheck').attr("checked", false);
 //  }
   pad.changeViewOption('showLineNumbers', false);
-}
+};
