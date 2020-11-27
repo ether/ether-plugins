41cd59e8314470d342169ad602862f9ccc0b061f
John McLear <john@mclear.co.uk>
2020-11-27 14:48:42 +0000

    fix

--- static/js/main.js
+++ static/js/main.js
@@ -3,6 +3,6 @@
  * This hook inserts a bunch of Javascripts and CSS links into the editor iframe. They are used for styling of and making the image icons displayed in the main pad text clickable.
  */
 exports.aceInitInnerdocbodyHead = function (hook_name, args, cb) {
-  args.iframeHTML.push('<script type="text/javascript" src="../static/plugins/ep_drag-and-drop/static/js/ace_inner.js"></script>');
+  args.iframeHTML.push('<script type="text/javascript" src="../static/plugins/ep_disable_drag_and_drop/static/js/ace_inner.js"></script>');
   return cb();
 };
