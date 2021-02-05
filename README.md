785966f4f2f9b1f48d3fdb8ef353cc4e4dd0d444
John McLear <john@mclear.co.uk>
2021-02-05 19:10:52 +0000

    working

---
+++ README.md
@@ -0,0 +1,5 @@
+# Disable Error Messages in Etherpad
+
+Disables the gritter popup errors messages so your users will never know how broken your install is <3 :)
+
+
--- static/js/disable_error_messages.js
+++ static/js/disable_error_messages.js
@@ -1,11 +1,11 @@
-var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
+'use strict';
 
-exports.suppress = function (hook_name, args, cb) {
-  setTimeout(() => {
-    $('#editorloadingbox').hide('Error messages suppressed by your system administrator');
-    $('#editorcontainerbox').append("<div id='suppresserror' style='text-align:center;margin-top:50px;'><h1>Error messages suppressed by your system administrator</h1></div>");
-  }, 1000);
-};
-exports.postAceInit = function (hook_name, args, cb) {
-  $('#suppresserror').hide();
+exports.postAceInit = (hookName, args, cb) => {
+  $('body').append(
+      `<style type="text/css">
+        #gritter-container > .error {
+          display:none !important;
+        }
+        </style>
+        `);
 };
