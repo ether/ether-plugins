0439aa860d1f886bbbbeeec05c0d6e6c81cd2a5e
John McLear <john@mclear.co.uk>
2021-02-05 18:12:19 +0000

    lint

--- static/js/disable_reset_authorship_colours.js
+++ static/js/disable_reset_authorship_colours.js
@@ -1,12 +1,12 @@
-var $ = require('ep_etherpad-lite/static/js/rjquery').$; // use jQuery
+'use strict';
 
-exports.postAceInit = function (hook_name, args, cb) {
+exports.postAceInit = (hookName, args, cb) => {
   // get clearauthorship item
   const item = $('li[data-key="clearauthorship"]');
   // check if the item is sorrounded by separators, then hide the previous one
-  if (item.prev('.separator').length == 1 &&
-		item.next('.separator').length == 1) {
-	  item.prev('.separator').remove();
+  if (item.prev('.separator').length === 1 &&
+    item.next('.separator').length === 1) {
+    item.prev('.separator').remove();
   }
   // hide the item
   item.remove();
