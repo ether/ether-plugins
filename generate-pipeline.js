3bb48753c6bec05cb593db327e91d219d5141485
SamTV12345 <40429738+samtv12345@users.noreply.github.com>
2025-04-08 22:47:37 +0200

    Added workflows

---
+++ generate-pipeline.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const {readdirSync, statSync} = require("node:fs");
+
+
+
+const generateContent = (path, )=>`name: Node.js Package
+on:
+  push:
+    branches: [ "main" ]
+    paths:
+      - "${path}/**"
+
+jobs:
+  backend:
+    uses: ether/ether-pipelines/.github/workflows/test-and-release-plugins.yml@main
+    secrets: inherit
+`
+
+for (let file of readdirSync('.')) {
+    const fullPath = path.join('./', file);
+    const stats = statSync(fullPath);
+    if (stats.isDirectory()) {
+        const content = generateContent(file);
+        const fileName = path.join('./.github/workflows/', `test-and-release-${file}.yml`);
+        console.log(`Creating ${fileName}`);
+        require('fs').writeFileSync(fileName, content);
+    }
+}
